3a303996e17a2c049b22389be9ec730e47c0ed31
momena <momena.salloum59@gmail.com>
2023-10-23 00:52:51 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+
+    return fetch("http://localhost:3000/users",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({ name, email })
+        })
+        .then(res => res.json())
+        .then(obj => {
+            // Manipulate HTML to add the ID of the data
+            document.body.innerHTML = `ID: ${obj.id}`;
+            return obj;
+        }) // Pass the data to the next then block})
+        .catch(error => {
+            document.body.innerHTML = `Error: ${error.message}`;
+        }
+        );
+
+}
+document.body.innerHTML = obj.id
+document.body.innerHTML = error.message
